c59ea5b2b5b5e6138d2300b4267e38a25fed76f3
Alcides Mignoso e Silva <alcidesmig@gmail.com>
2023-06-15 16:34:34 -0300

    feat: add internal models

--- internal/models/github.go
+++ internal/models/github.go
@@ -1,7 +1,9 @@
 package models
 
+// GithubMetadata represents some metadata of a GitHub repository.
 type GithubMetadata struct {
 	Owner          string
 	RepositoryName string
 	Path           string
+	Branch         string
 }
